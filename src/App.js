2b0f56093d5b9f159f8012ebdccee237b821933b
Zhakshylyk <zhakshylykkozhobekov@gmail.com>
2022-01-07 17:18:45 +0600

    third

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import "./App.css";
 import Todolist from "./Todolist";
 import AddList from "./AddList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Jumbotron as Jumbo } from "reactstrap";
 const App = () => {
   const [value, setValue] = React.useState("");
   const [todo, setTodo] = React.useState(["Buy a new laptop"]);
@@ -14,25 +15,33 @@ const App = () => {
     e.preventDefault();
   };
 
-  let ifEmpty = 'The field must not be empty'
+  let ifEmpty = "The field must not be empty";
 
-  const addList = () => {
-    value === '' ? alert(ifEmpty) : setTodo(() => [...todo, value]);
-    setValue("");
+  const addList = (e) => {
+    // if (e.key === "Enter") {
+      value === "" ? alert(ifEmpty) : setTodo(() => [...todo, value]);
+      setValue("");
+    
+  };
+  const styles = {
+    forUl: {
+      padding: "0px",
+      margin: "0px, 2rem",
+    },
   };
   return (
-    <div className="App">
-      <AddList
-        formSubmit={formSubmit}
-        valueInput={value}
-        valueChange={valueChange}
-        addTodo={addList}
-        ifValueEmpty={ifEmpty}
-      />
-      <ul>
-        <Todolist todos={todo} />
-      </ul>
-    </div>
+      <div className="App">
+        <AddList
+          formSubmit={formSubmit}
+          valueInput={value}
+          valueChange={valueChange}
+          addTodo={addList}
+          ifValueEmpty={ifEmpty}
+        />
+        <ul style={styles.forUl}>
+          <Todolist todos={todo} />
+        </ul>
+      </div>
   );
 };
 
